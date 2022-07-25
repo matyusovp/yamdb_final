a973d39fc2178554f26ff477d56728d29b669bc4
Павел Матюсов <matyusovp@gmail.com>
2022-07-25 14:01:06 +0300

    flake8_tests

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv(),
-        'USER': os.getenv(),
-        'PASSWORD': os.getenv(),
-        'HOST': os.getenv(),
-        'PORT': os.getenv()
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
