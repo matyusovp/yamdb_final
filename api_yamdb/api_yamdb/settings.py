787cf06348366175571653682ad4351360222bae
Павел Матюсов <matyusovp@gmail.com>
2022-07-28 12:10:06 +0300

    sborka

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,8 +3,6 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
